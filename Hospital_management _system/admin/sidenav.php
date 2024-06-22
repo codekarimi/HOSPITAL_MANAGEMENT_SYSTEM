72ac8e617b46c52deb6895e90c168e705a7352de
zeuss254 <ckarimi676@gmail.com>
2024-06-22 17:54:12 +0300

    Doctor's Sidebar

---
+++ Hospital_management _system/admin/sidenav.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="list-group bg-info" style="height: 90vh;">
+        <a href="http://localhost:8080/Hospital%20management%20system/admin/index.php" class="list-group-item list-group-item-action bg-info text-center text-white">DashBoard</a>
+        <a href="http://localhost:8080/Hospital%20management%20system/admin/profile.php" class="list-group-item list-group-item-action bg-info text-center text-white">Profile</a>
+        <a href="http://localhost:8080/Hospital%20management%20system/admin/admin.php" class="list-group-item list-group-item-action bg-info text-center text-white">Administrators</a>
+        <a href="http://localhost:8080/Hospital%20management%20system/admin/doctor.php" class="list-group-item list-group-item-action bg-info text-center text-white">Doctors</a>
+        <a href="" class="list-group-item list-group-item-action bg-info text-center text-white">Patient</a>
+    </div>
+
+    <!-- ends -->
+
+</body>
+
+</html>
