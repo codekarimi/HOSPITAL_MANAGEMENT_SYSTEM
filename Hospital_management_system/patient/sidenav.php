148e831b7043f2b721edba9692510104f0b783e2
zeuss254 <ckarimi676@gmail.com>
2024-06-25 11:54:09 +0300

    Patient Sidenav

---
+++ Hospital_management_system/patient/sidenav.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="list-group bg-info" style="height: 90vh;">
+        <a href="http://localhost:8080/Hospital%20management%20system/patient/index.php" class="list-group-item list-group-item-action bg-info text-center text-white">DashBoard</a>
+        <a href="http://localhost:8080/Hospital%20management%20system/patient/profile.php" class="list-group-item list-group-item-action bg-info text-center text-white">Profile</a>
+        <a href="#" class="list-group-item list-group-item-action bg-info text-center text-white">Book Appointment</a>
+        <a href="#" class="list-group-item list-group-item-action bg-info text-center text-white">Report</a>
+        <a href="#" class="list-group-item list-group-item-action bg-info text-center text-white">Invoice</a>
+    </div>
+
+    <!-- ends -->
+
+</body>
+
+</html>
